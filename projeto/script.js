e7225d77a90eea86ae7d4999ccfc1633fba89bab
Henrique Ferreira Cardoso <129521477+HenriqueFerC@users.noreply.github.com>
2025-04-15 16:45:59 -0300

    Subindo mais algumas coisas

--- projeto/script.js
+++ projeto/script.js
@@ -18,36 +18,49 @@ botao2.addEventListener("click", function() {
     let tarefa = document.querySelector("#tarefas");
     let tarefas = tarefa.value;
 
-    if(tarefas.length == 0 || tarefas.length >= 10){
-        tarefa.value = "";
-        meses.selectedIndex = 0;
-        return;
-    }
     
     let meses = document.querySelector("#mesesTarefa");
     let mesesTarefa = meses.value;
-
+    
+    if(tarefas.length == 0 || tarefas.length >= 20){
+        tarefa.value = "";
+        meses.value = undefined;
+        return;
+    }
+    
     if(mesesTarefa == ""){  
         tarefa.value = "";
         return;
     }
 
-    if(mesesTarefa.length > 0 && tarefas.length == 0){
-        meses.selectedIndex = 0;
-        return;
-    }
-
+    
     let tarefaListagem = document.querySelector("#listaDeTarefa");
     let listaDeTarefas = document.createElement("li");
+    listaDeTarefas.classList.add("lista");
 
     tarefaListagem.appendChild(listaDeTarefas);
 
     
     tarefa.value = "";
-    meses.selectedIndex = 0;
+    meses.value = undefined;
 
-    
     listaDeTarefas.textContent = "Essa é a tarefa: " + tarefas + ". Com prazo de: " + mesesTarefa;
 
 
 });
+
+let pagina2 = document.querySelector(".link1");
+
+pagina2.addEventListener("click", function(event){
+    event.preventDefault();
+    pagina2.href = "calculadora.html";
+    window.location.href = "calculadora.html";
+})
+
+let voltar = document.querySelector("#volta");
+
+voltar.addEventListener("click", function(event){
+    event.preventDefault();
+    voltar.href = "index.html"
+    window.location.href = "index.html";
+})
