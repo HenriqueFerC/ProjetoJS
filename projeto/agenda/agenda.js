47296ed815effc90a2eaf7797c23709d85f10519
Henrique Ferreira Cardoso <129521477+HenriqueFerC@users.noreply.github.com>
2025-04-18 12:43:35 -0300

    Adding conditional uses and partial localStorage

--- projeto/agenda/agenda.js
+++ projeto/agenda/agenda.js
@@ -17,62 +17,79 @@ botaoVoltar?.addEventListener("click", function(event){
 
 const botaoTarefa = document.querySelector("#botaoTarefa");
 
+const localStorageItems = localStorage;
+
 botaoTarefa?.addEventListener("click", function(){
-    let tarefa = document.querySelector("#tarefa");
-    let tarefaValue = tarefa.value.toUpperCase();
+    const tarefa = document.querySelector("#tarefa");
+    const tarefaValue = tarefa.value.toUpperCase();
 
     
-    let horario1 = document.querySelector("#horario1");
-    let horario1Value = horario1.value;
+    const horario1 = document.querySelector("#horario1");
+    const horario1Value = horario1.value;
 
-    let horario2 = document.querySelector("#horario2");
-    let horario2Value = horario2.value;
+    const horario2 = document.querySelector("#horario2");
+    const horario2Value = horario2.value;
 
     let erro = document.querySelector("#error");
+    
+    if(validationNull()){
+        tarefa.value = "";
+        horario1.value = "";
+        horario2.value = "";
+        
+        erro.textContent = "Nenhum campo pode ser vazio!";
+  
+        return;
+    }
 
-   if(tarefaValue == "" || horario1Value == "" || horario2Value == ""){
-       tarefa.value = "";
-       horario1.value = "";
-       horario2.value = "";
-       
-       erro.textContent = "Nenhum campo pode ser vazio!";
+    function validationNull(){
+        if(tarefaValue == "" || horario1Value == "" || horario2Value == ""){
+            return true;
+        }
+        return false;
+    }
 
-       return;
-   }
+    function getHour(hourTime){
+        const [hour, minute] = hourTime.split(":");
+        const dateFormat = new Date(anoAtual, mesAtual, diaAtual, hour,minute,0);
+        const dataFormatValue = dateFormat.getTime();
+        return dateFormat;
+    }
 
-   function getHoras(hour){
-    const [horas, minutos] = hour.split(":");
-    return horas * 60 + minutos;
-   }
-
-   if(horario1Value == horario2Value){
-       tarefa.value = "";
-       horario1.value = "";
-       horario2.value = "";
-
-       erro.textContent = "Horários precisam ser diferentes!";
-
-       return;
-   }
-
-   if(getHoras(horario1Value) < getHoras(horario2Value)){
-       tarefa.value = "";
-       horario1.value = "";
-       horario2.value = "";
-
-       erro.textContent = "Horário 1 precisa ser maior do que o Horário 2!";
-
-       return;
-   }
+    if(getHour(horario1Value) >= getHour(horario2Value)){
+        tarefa.value = "";
+        horario1.value = "";
+        horario2.value = "";
+  
+        erro.textContent = "Horário 2 precisa ser maior do que o Horário 1!";
+  
+        return;
+    }
 
     const listaDeTarefas = document.querySelector("#listaTarefa");
     const itemLista = document.createElement("li");
     itemLista.classList.add("lista");
     itemLista.textContent = `Tarefa: ${tarefaValue} horário ${horario1Value} às ${horario2Value}`
+    
 
     listaDeTarefas.appendChild(itemLista);
+
+    const itemStorage = {
+        tarefaValue,
+        horario1Value,
+        horario2Value
+        };
+    const itemStorageJSON = JSON.stringify(itemStorage);
+
+
+
     tarefa.value = "";
     horario1.value = "";
     horario2.value = "";
     erro.textContent = "";
+
+
+
+
+    localStorageItems.setItem('itemStorageJSON', itemStorageJSON);
 })
