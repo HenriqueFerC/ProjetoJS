acc4ef2e395f83b8bd32321d1e89e3b4ec2ec31d
Henrique Ferreira Cardoso <129521477+HenriqueFerC@users.noreply.github.com>
2025-04-29 16:54:00 -0300

    Cleaning code and trying to simplify Update Func

--- agenda/agenda.js
+++ agenda/agenda.js
@@ -31,11 +31,11 @@ botaoVoltar?.addEventListener("click", function (event) {
 const localStorageToParse = localStorage.getItem('itemStorageJSON');
 const arrayTasks = localStorageToParse ? JSON.parse(localStorageToParse) : [];
 
-arrayTasks.forEach((task) => {
+function listTasks(tarefaValue, horario1Value, horario2Value) {
     const listaDeTarefas = document.querySelector("#listaTarefa");
     const itemLista = document.createElement("li");
     itemLista.classList.add("lista");
-    itemLista.textContent = `Tarefa: ${task.tarefaValue} horário: ${task.horario1Value} às ${task.horario2Value}`;
+    itemLista.innerHTML = `Tarefa: <span id="span">${tarefaValue}</span> horário: ${horario1Value} às ${horario2Value}`;
     const buttonList = document.createElement("button");
     const updateList = document.createElement("button");
     buttonList.classList.add("buttonList");
@@ -45,6 +45,16 @@ arrayTasks.forEach((task) => {
     listaDeTarefas.appendChild(itemLista);
     itemLista.appendChild(updateList);
     itemLista.appendChild(buttonList);
+}
+
+function resetValues(tarefa, horario1, horario2){
+    tarefa.value = "";
+    horario1.value = "";
+    horario2.value = "";
+}
+
+arrayTasks.forEach((task) => {
+    listTasks(task.tarefaValue, task.horario1Value, task.horario2Value);
 })
 
 function validationNull(tarefaValue, horario1Value, horario2Value) {
@@ -81,11 +91,7 @@ botaoTarefa?.addEventListener("click", function () {
             const dataTask1 = new Date(anoAtual, mesAtual, diaAtual, taskHour1, taskMinute1, 0).getTime();
             const dataTask2 = new Date(anoAtual, mesAtual, diaAtual, taskHour2, taskMinute2, 0).getTime();
 
-            console.log("hr 1 " + dataHorario1);
-            console.log("tk 1 " + dataTask1);
-            console.log("tk 2 " + dataTask2);
-
-            if (dataHorario1 <= dataTask1 && dataHorario1 <= dataTask2) {
+            if (dataHorario1 <= dataTask1 && dataHorario2 >= dataTask2) {
                 validationHourBoolean = true;
             }
             else if (dataHorario1 <= dataTask1 && dataHorario2 >= dataTask2) {
@@ -100,9 +106,7 @@ botaoTarefa?.addEventListener("click", function () {
     validationHour(horario1Value, horario2Value);
 
     if (validationHourBoolean) {
-        tarefa.value = "";
-        horario1.value = "";
-        horario2.value = "";
+        resetValues(tarefa, horario1, horario2);
 
         erro.textContent = "Existe uma agenda cadastrada no intervalo informado!";
 
@@ -110,9 +114,7 @@ botaoTarefa?.addEventListener("click", function () {
     }
 
     if (validationNull(tarefaValue, horario1Value, horario2Value)) {
-        tarefa.value = "";
-        horario1.value = "";
-        horario2.value = "";
+        resetValues(tarefa, horario1, horario2);
 
         erro.textContent = "Nenhum campo pode ser vazio!";
 
@@ -126,41 +128,25 @@ botaoTarefa?.addEventListener("click", function () {
     }
 
     if (getHour(horario1Value) >= getHour(horario2Value)) {
-        tarefa.value = "";
-        horario1.value = "";
-        horario2.value = "";
+        resetValues(tarefa, horario1, horario2);
 
         erro.textContent = "Horário 2 precisa ser maior do que o Horário 1!";
 
         return;
     }
 
-    const listaDeTarefas = document.querySelector("#listaTarefa");
-    const itemLista = document.createElement("li");
-    const buttonList = document.createElement("button");
-    const updateList = document.createElement("button");
-    itemLista.classList.add("lista");
-    buttonList.classList.add("buttonList");
-    updateList.classList.add("updateList");
-    buttonList.textContent = "Excluir";
-    updateList.textContent = "Atualizar";
-    itemLista.textContent = `Tarefa: ${tarefaValue} horário: ${horario1Value} às ${horario2Value}`;
-
-    listaDeTarefas.appendChild(itemLista);
-    itemLista.appendChild(updateList);
-    itemLista.appendChild(buttonList);
+    listTasks(tarefaValue, horario1Value, horario2Value);
 
     const itemStorage = {
         tarefaValue,
         horario1Value,
         horario2Value
     };
+    
     arrayTasks.push(itemStorage);
     const itemStorageJSON = JSON.stringify(arrayTasks);
 
-    tarefa.value = "";
-    horario1.value = "";
-    horario2.value = "";
+    resetValues(tarefa, horario1, horario2);
     erro.textContent = "";
 
     localStorage.setItem('itemStorageJSON', itemStorageJSON);
@@ -197,18 +183,11 @@ function atualizarEventos() {
 
             arrayTasks[index].tarefaValue = updatedTask;
 
-            const listaDeTarefas = document.querySelector("#listaTarefa");
-            listaDeTarefas.children[index].textContent = `Tarefa: ${updatedTask} horário: ${arrayTasks[index].horario1Value} às ${arrayTasks[index].horario2Value}`;
 
-            const itemLista = document.querySelector(".lista");
-            const buttonList = document.createElement("button");
-            const updateList = document.createElement("button");
-            buttonList.classList.add("buttonList");
-            updateList.classList.add("updateList");
-            buttonList.textContent = "Excluir";
-            updateList.textContent = "Atualizar";
-            itemLista.appendChild(buttonList);
-            itemLista.appendChild(updateList);
+            const itemLista = document.querySelectorAll("#lista");
+            const span = document.querySelectorAll("#span");
+            item.children[index] = `${updatedTask}`
+
 
             const itemStorageJSON = JSON.stringify(arrayTasks);
             localStorage.setItem('itemStorageJSON', itemStorageJSON);
